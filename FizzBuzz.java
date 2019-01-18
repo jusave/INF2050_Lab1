13c6d540591f303fb51ed1d28aeeafe035fce858
jusave <sanchez_velasco.julian@courrier.uqam.ca>
2019-01-18 15:00:53 -0500

    modifications au fichier FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i<100; i++) {
+		for (int i = 1; i<=200; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
 				System.out.println("FizzBuzz");
 			} else if (i % 3 == 0) {
